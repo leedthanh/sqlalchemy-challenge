0547247eb41d39d59b07b64dc209e17045447522
leedthanh <leedthanh@gmail.com>
2023-08-20 00:04:48 -0500

    hawaii climate api

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,54 @@
 # Import the dependencies.
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+from flask import Flask, jsonify
 
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def welcome():
+    """ list all available api routes."""
+    return (
+        f"Welcome to Hawaii climate app. <br/>"
+        f"Available Route.<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start/2010-01-01<br/>"
+        f"/api/v1.0/start/2010-01-01/end/2017-08-23"
+    )
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
