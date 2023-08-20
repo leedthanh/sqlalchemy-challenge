acf9d13cd96f578e0e9b61f17d6465ad0ea6d5c8
leedthanh <leedthanh@gmail.com>
2023-08-20 00:37:18 -0500

    hawaii climate api precipitation route

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -13,19 +13,18 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
-Base.prepare(autoload_with=engine)
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
 measurement = Base.classes.measurement
 station = Base.classes.station
 # Create our session (link) from Python to the DB
 session = Session(engine)
-
 #################################################
 # Flask Setup
 #################################################
@@ -49,6 +48,24 @@ def welcome():
         f"/api/v1.0/start/2010-01-01/end/2017-08-23"
     )
 
-@app.route("/api/v1.0/stations")
+@app.route("/api/v1.0/precipitation")
 def stations():
+    # Create our session (link) from Python to the DB
     session = Session(engine)
+
+    # query date and prcp from measurement table
+    prcp_date = session.query(measurement.date, measurement.prcp).all()
+    session.close()
+
+    precipitation = []
+    for date, prcp in prcp_date:
+        date_dict = {}
+        date_dict[date] = prcp
+        precipitation.append(date_dict)    
+    return jsonify(precipitation)
+
+
+#close session and app run debug
+session.close()
+if __name__ == '__main__':
+    app.run(debug=True)
